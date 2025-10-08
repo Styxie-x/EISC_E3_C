30cb3993d3a4d2c8b518447d283afcfa9dc4dcf8
debian_aetherlinx <asad.19.08.02@gmail.com>
2025-10-08 12:06:00 +0200

    TD2

---
+++ TD2/lib/Module_2/Module_2.h
@@ -0,0 +1,12 @@
+#ifndef MODULE_2_H
+#define MODULE_2_H
+
+#include <time.h>
+
+#include <iostream> // nput stream and output stream objects, e.g. std::cout, std::cin.
+#include <stdio.h>  // input/output functionalities through the use of printf() and scanf().
+
+
+void Module_2();
+
+#endif
